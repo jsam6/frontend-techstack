a51bcddfb411b817d3b0fd7be3f67c6c79669edd
sam <cjsam6@yahoo.com>
2020-04-25 16:41:00 +0800

    updated.scss files into diff files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import Header from './components/Header';
 import './scss/app.scss';
 
-const app = () => {
-    document.getElementById('header').innerHTML = Header();
-}
+// const app = () => {
+//     document.getElementById('header').innerHTML = Header();
+// }
 
-// init app
-app();
+// // init app
+// app();
 console.log('nice')
---
+++ src/scss/README.md
@@ -0,0 +1,7 @@
+'_' before a filename.scss indicates that it is a 'partial' file and is not to be compiled into a .css file. It is only to be imported into another .scss file.
+
+& : shortcut for parents naming
+
+% : shared styles
+
+.scss allows to use functions if else.
