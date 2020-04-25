410d3992af17648b49a120aa730ee4c5887dda45
sam <cjsam6@yahoo.com>
2020-04-25 14:31:46 +0800

    appling scss

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+import Header from './components/Header';
+import './scss/app.scss';
+
+const app = () => {
+    document.getElementById('header').innerHTML = Header();
+}
+
+// init app
+app();
 console.log('nice')
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    const template = `
+        <header>
+            <h1>Paper Page</h1>
+            <p>lorem ipsum dolor sit amet, consectetur adip</p>
+        </header>
+    `
+    return template;
+}
+export default Header;
